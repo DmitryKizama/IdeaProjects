d4770b85ec6c0987800ac9a85b217f2e34384c48
DmitryKizama <dmitrikkizama@gmail.com>
2016-04-27 23:05:22 +0300

    inner class

--- src/com/training/pacage/MinClass.java
+++ src/com/training/pacage/MinClass.java
@@ -3,59 +3,28 @@ package com.training.pacage;
 
 class Outer {
 
-    public String word = "Outer";
-
-    private class InnerPrivate {
-        public static final int field = 23;
-        private String word;
-
-        InnerPrivate() {
-            word = "Inner Private";
-        }
-
-        public String getWord() {
-            return word;
-        }
-    }
-
-    String getWord() {
-        return new InnerPrivate().getWord();
-    }
-
-    public InnerPrivate getInnerPrivate(){
-        return new InnerPrivate();
-    }
-
-    public InnerPublic getInnerPublic(){
-        return new InnerPublic();
-    }
-
-    public class InnerPublic {
-        public static final int field = 23;
-        private String word;
-
-        InnerPublic() {
-            word = "Inner Private";
-        }
-
-        public String getWord() {
-            return word;
-        }
-    }
-
 }
+
+abstract class D{}
+class Iner extends Outer{
+    D makeD(){
+        return new D(){
+            private String j = "sds";
+            public String k = " dsdgdsf";
+            public String getK(){
+                return k;
+            }
+        };
+    }
+}
+
+
 
 public class MinClass {
 
-
     public static void main(String[] args) {
-        Outer outer = new Outer();
-
-        Outer.InnerPublic i = outer.getInnerPublic();
-        outer.getInnerPrivate();
-        System.out.println(Outer.InnerPublic.field);
-        System.out.println(outer.getWord());
-        System.out.println(i.getWord());
+        Iner iner = new Iner();
+        iner.makeD();
     }
 
 }
